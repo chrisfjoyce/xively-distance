600ff71e80698bafeef008040e2d6fb3c93bb1ab
Gabriel Rea Velasco <gabriel.rea@tierconnect.com>
2014-01-13 13:29:53 +0000

    Additional git information

--- session_init.sh
+++ session_init.sh
@@ -2,6 +2,7 @@ ln -s ~/node_modules_in_home /vagrant/node_modules
 
 echo "### Remote Folder in \\10.10.10.10 user:vagrant pass:vagrant ###"
 echo "### Git init"
+echo '   git clone https://github.com/tierconnect/xively-distance.git'
 echo '   git config --global color.ui auto'
 echo '   git config --global user.name "abc'
 echo '   git config --global user.email "abc@tierconnect.com"'
