0b0e33313061ab8071251b757288371e2f2c53ec
Gabriel Rea Velasco <gabriel.rea@tierconnect.com>
2014-01-13 13:01:46 +0000

    Vagrant + init conf

--- session_init.sh
+++ session_init.sh
@@ -1,10 +1,14 @@
 ln -s ~/node_modules_in_home /vagrant/node_modules
 
 echo "### Remote Folder in \\10.10.10.10 user:vagrant pass:vagrant ###"
-echo "### Some Nodejs Tool notes ###"
+echo "### Git init"
+echo '   git config --global color.ui auto'
+echo '   git config --global user.name "abc'
+echo '   git config --global user.email "abc@tierconnect.com"'
+echo "### Some Nodejs Tool notes #"
 echo "   $ bower install"
-echo "   $ npm install --no-bin-links"
-echo "   $ npm update --no-bin-links"
+echo "   $ npm install"
+echo "   $ npm update"
 echo "   $ grunt test"
-echo "   $ grunt serve #start server"
-echo "   $ grunt #Create dist package"
+echo "   $ grunt serve  #start server"
+echo "   $ grunt        #Create dist package"
