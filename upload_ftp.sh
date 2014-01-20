921d3fd5c1cf154a789cc0cc3377dbf46660f59b
Gabriel Rea Velasco <gabriel.rea@tierconnect.com>
2014-01-20 15:38:08 +0000

    Upload webpage to TC servers

--- upload_ftp.sh
+++ upload_ftp.sh
@@ -1,6 +1,18 @@
 sudo apt-get install lftp wput
 
+grunt clean
+grunt compass
+
+rm -rf /tmp/app
+mkdir /tmp/app
+
+cp -r app /tmp
+
+cp .tmp/styles/main.css /tmp/app/styles
+
 printf %s $(date --date="4 hours ago" +%b_%d_%Y@%H_%M_%S) > /tmp/date.txt
+
+cd /tmp
 zip -r /tmp/xively-iostp-$(cat /tmp/date.txt).zip app
 wput /tmp/xively-iostp-$(cat /tmp/date.txt).zip ftp://vagrant:vagrant@10.100.0.78/xively-daily-builds/xively-iostp-$(cat /tmp/date.txt).zip
 
@@ -8,7 +20,7 @@ HOST="10.100.0.78"
 USER="vagrant"
 PASS="vagrant"
 FTPURL="ftp://$USER:$PASS@$HOST"
-LCD="app"
+LCD="/tmp/app"
 RCD="/www"
 DELETE="--delete"
 time lftp -c "set ftp:list-options -a;
