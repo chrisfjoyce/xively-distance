0a5d37f01f71f445bff409201367baaff56d0468
ronald-valverde-tierconnect-com <ronald.valverde@tierconnect.com>
2014-01-20 15:43:56 +0000

    Included modal view for schools

--- app/scripts/controllers/by_data_type_controller.js
+++ app/scripts/controllers/by_data_type_controller.js
@@ -1,7 +1,7 @@
 /*jshint sub:true*/
 'use strict';
 
-var ByDataTypeCtrl = function ($scope) {
+var ByDataTypeCtrl = function ($scope,$modal) {
   initXivelyData();
   registerXivelyGetData(function(){
     $scope.$apply(
@@ -11,9 +11,25 @@ var ByDataTypeCtrl = function ($scope) {
     );
   });
 
-  $scope.selectedDatastream = {};  
+  $scope.selectedDatastream = {};
+
+  $scope.open = function (datastream) {
+    var modalInstance = $modal.open({
+      templateUrl: 'views/bySchool.html',
+      scope: $scope,
+    });
+    console.log(datastream);
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+    $scope.modal = modalInstance;
+  };
 };
 
-ByDataTypeCtrl.$inject = ['$scope'];
+
+
+ByDataTypeCtrl.$inject = ['$scope','$modal'];
 var app = angular.module('xivelyIostpApp');
 app.controller('ByDataTypeCtrl', ByDataTypeCtrl);
