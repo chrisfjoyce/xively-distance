bb59f07a69e2e8b10fdecf0dcf319481115b0f97
Gabriel Rivera <gabriel.rivera@tierconnect.com>
2014-01-23 16:00:29 +0000

    Add some functionalities to the data types popup.

--- app/scripts/controllers/by_data_stream_popup_controller.js
+++ app/scripts/controllers/by_data_stream_popup_controller.js
@@ -3,6 +3,38 @@
 
 var ByDataStreamPopUpCtrl = function ($scope) {
   $scope.dataStreams=_datastreamsBySchool[$scope.selectedSchool];
+  $scope.active = 0;
+  $scope.inactive = 0;
+  //TODO garivera remove simulation of active/inactive
+  //simulate active/inactive
+  for (var i = 0; i < $scope.dataStreams.length; i++) {
+    if (i % 3 == 0) {
+      $scope.dataStreams[i].active = true;
+      $scope.active++;
+    } else {
+      $scope.dataStreams[i].active = false;
+      $scope.inactive++;
+    }
+  }
+  //end simulate active/inactive
+
+  $scope.dataStreamsSelected = 0;
+  for (var selectedDataStream in $scope.selectedDatastreamsBySchool) {
+    for (i = 0; i < $scope.dataStreams.length; i++) {
+      if (selectedDataStream == $scope.dataStreams[i].id && $scope.selectedDatastreamsBySchool[selectedDataStream][$scope.dataStreams[i].deviceId]) {
+        $scope.dataStreamsSelected++;
+      }
+    }
+  }
+
+  $scope.setSelected = function($event) {
+    var checkbox = $event.target;
+    if (checkbox.checked) {
+      $scope.dataStreamsSelected++;
+    } else {
+      $scope.dataStreamsSelected--;
+    }
+  };
 };
 
 ByDataStreamPopUpCtrl.$inject = ['$scope'];
