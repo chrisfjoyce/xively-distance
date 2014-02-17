bc3b62740d957a31b81089050c7e1491242d0adc
Gabriel Rivera <gabriel.rivera@tierconnect.com>
2014-02-17 18:16:38 +0000

    Get the data for the CSV by chunks.

--- app/services/merge_xively_csv.php
+++ app/services/merge_xively_csv.php
@@ -1,4 +1,65 @@
 <?php
+
+$INTERVAL_VALUES = array(
+  0 => array(
+    'index' => 0,
+    'interval' => 0,
+    'maximum' => 6
+  ),
+  1 => array(
+    'index' => 1,
+    'interval' => 30,
+    'maximum' => 12
+  ),
+  2 => array(
+    'index' => 2,
+    'interval' => 60,
+    'maximum' => 24
+  ),
+  3 => array(
+    'index' => 3,
+    'interval' => 300,
+    'maximum' => 120
+  ),
+  4 => array(
+    'index' => 4,
+    'interval' => 900,
+    'maximum' => 336
+  ),
+  5 => array(
+    'index' => 5,
+    'interval' => 1800,
+    'maximum' => 744
+  ),
+  6 => array(
+    'index' => 6,
+    'interval' => 3600,
+    'maximum' => 744
+  ),
+  7 => array(
+    'index' => 7,
+    'interval' => 10800,
+    'maximum' => 2160
+  ),
+  8 => array(
+    'index' => 8,
+    'interval' => 21600,
+    'maximum' => 4320
+  ),
+  9 => array(
+    'index' => 9,
+    'interval' => 43200,
+    'maximum' => 8640
+  ),
+  10 => array(
+    'index' => 10,
+    'interval' => 86400,
+    'maximum' => 8640
+  )
+);
+$INTERVAL_CURRENT_INDEX = 8;
+
+
 //$link = mysql_connect('localhost','root','') or die('Could not connect to MySQL: ' . mysql_error());
 //$rv = mysql_select_db('xively-iostp', $link) or die('Could not connect to Xively database: ' . mysql_error());
 
@@ -32,11 +93,38 @@ foreach ($devicesByDatastream as $datastreamLabel => $datastream) {
    $schools = Array();
    //print_r($datastream);
    foreach($devices as $device){
-      array_push($contents,explode("\n", file_get_contents(sprintf('http://api.xively.com/v2/feeds/%d/datastreams/%s.csv?interval=21600&start=%s&end=%s&interval_type=discrete&x-apikey=LZ8CcFmj2huPno20yShkEGlm3QQAiuiMYsLQOjHEQpWOSzDs',$device->id,$device->datastreamId,$datastream->start_date,$datastream->end_date))));
+      // array_push($contents,explode("\n", file_get_contents(sprintf('http://api.xively.com/v2/feeds/%d/datastreams/%s.csv?interval=21600&start=%s&end=%s&interval_type=discrete&x-apikey=LZ8CcFmj2huPno20yShkEGlm3QQAiuiMYsLQOjHEQpWOSzDs',$device->id,$device->datastreamId,$datastream->start_date,$datastream->end_date))));
+      $timeToAdd = $INTERVAL_VALUES[$INTERVAL_CURRENT_INDEX]['maximum'];
+      $endDate = new DateTime($datastream->end_date);
+      $newStartDate = new DateTime($datastream->start_date);
+      $newEndDate = clone $newStartDate;
+      $newEndDate->modify("+".$timeToAdd." hours");
+      $contentsTmp = array();
+      while ($newStartDate < $endDate) {
+         $newStartDateISO = $newStartDate->format("c");
+         $newEndDateISO = $newEndDate->format("c");
+         $data = explode("\n", file_get_contents(sprintf('http://api.xively.com/v2/feeds/%d/datastreams/%s.csv?interval=%s&start=%s&end=%s&x-apikey=LZ8CcFmj2huPno20yShkEGlm3QQAiuiMYsLQOjHEQpWOSzDs',$device->id,$device->datastreamId,$INTERVAL_VALUES[$INTERVAL_CURRENT_INDEX]['interval'],$newStartDateISO,$newEndDateISO)));
+         for ($i = 0; $i < count($data); $i++) {
+            if ($data[$i] != "") {
+               array_push($contentsTmp, $data[$i]);
+            }
+         }
+         $newStartDate->modify("+".$timeToAdd." hours");
+         $newEndDate->modify("+".$timeToAdd." hours");
+         if ($newEndDate > $endDate) {
+           $newEndDate = clone $endDate;
+         }
+      }
       array_push($schools, $device->schoolName);
+      array_push($contents, $contentsTmp);
       $cols++;
    }
-   $rows = count($contents[0]);
+
+   $rows = 0;
+   for ($i = 0; $i < count($contents); $i++) {
+      if ($rows < count($contents[$i]))
+      $rows = count($contents[$i]);
+   }
 
    echo $datastreamLabel . "\n";
    for($col=0;$col<$cols;$col++){
@@ -45,7 +133,11 @@ foreach ($devicesByDatastream as $datastreamLabel => $datastream) {
    echo "\n";
    for($row=0;$row<$rows;$row++){
       for($col=0;$col<$cols;$col++){
-         echo $contents[$col][$row] . ',,';
+         if ($contents[$col][$row] == '') {
+            echo ',,,';
+         } else {
+            echo $contents[$col][$row] . ',,';
+         }
       }
       echo "\n";
    }
